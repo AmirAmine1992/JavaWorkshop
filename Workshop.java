35e937b90236396d7c834242216cc37b8f4149f3
AmirAmine1992 <33393923+AmirAmine1992@users.noreply.github.com>
2018-04-15 16:56:23 +0100

    Add files via upload

---
+++ Workshop.java
@@ -0,0 +1,15 @@
+
+public class Workshop {
+	/**
+	* @param args
+	*/
+	public static void main(String[] args) {
+		Personne unePersonne = new Personne();
+		unePersonne.nom = "Diet"; 
+		unePersonne.prenom = "MrBougadoum"; 
+		unePersonne.direBonjour();
+	}
+	}
+
+
+
